a35017a7d611dcdd8340bb902fb94641b9510942
CallMeCalvin808 <vikurniadjaja@s.sfusd.edu>
2018-10-17 10:05:25 -0700

    Completed assignment

--- Farm.java
+++ Farm.java
@@ -1,4 +1,16 @@
-class Farm 
-{     
-   //your code here
+class Farm {     
+  private Animal[] aBunchOfAnimals = new Animal[3];
+  
+  public Farm() {
+    aBunchOfAnimals[0] = new NamedCow("cow", "Jeff", "moo");
+    aBunchOfAnimals[1] = new Chick("chick", "clock", "cheep cheep");
+    aBunchOfAnimals[2] = new Pig("pig", "onk");
+  }
+  
+  public void animalSounds() {
+    for (int i = 0; i < aBunchOfAnimals.length; i++)
+      System.out.println(aBunchOfAnimals[i].getType() + " goes " + aBunchOfAnimals[i].getSound());
+      
+    System.out.println("This cow nama " + ((NamedCow)aBunchOfAnimals[0]).getName());
+  }
 }
